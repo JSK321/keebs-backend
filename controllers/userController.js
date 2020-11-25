e9a9584d62c93dd657d033f968a912d49a9a6a14
Jae Kim <jaeshinkim321@gmail.com>
2020-11-25 14:26:40 -0800

    log in router

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const bcrypt = require('bcrypt');
 
 router.get("/", (req, res) => {
     db.User.findAll().then(dbUsers => {
         res.json(dbUsers)
+    }).catch(err => {
+        console.log(err)
+        res.status(500).end()
     })
 });
 
@@ -15,6 +19,26 @@ router.post("/", (req, res) => {
         password: req.body.password
     }).then(newUser => {
         res.json(newUser)
+    }).catch(err => {
+        console.log(err)
+        res.status(500).end()
+    })
+})
+
+router.post("/login", (req,res) => {
+    db.User.findOne({
+        where: {
+            email: req.body.email,
+        }
+    }).then(foundUser => {
+        if(!foundUser){
+            return res.status(404).send("User not found")
+        }
+        if(bcrypt.compareSync(req.body.password, foundUser.password)) {
+            return res.status(200).send("login successful")
+        } else {
+            return res.status(403).send("incorrect password")
+        }
     })
 })
 
