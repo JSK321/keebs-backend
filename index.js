e45b8133a1d4954f8a4a09388ac7368806cc2282
Jae Kim <jaeshinkim321@gmail.com>
2021-01-01 12:55:00 -0800

    changing sequelize datasynce to false to push to heroku main

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cors({
 
 app.use('/', allRoutes);
 
-db.sequelize.sync({ force: true }).then(function () {
+db.sequelize.sync({ force: false }).then(function () {
     app.listen(PORT, function () {
         console.log('App listening on PORT ' + PORT);
     });
